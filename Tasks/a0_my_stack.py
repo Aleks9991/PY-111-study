b4d8c14631ec039950add17c59928086316d663c
ALELAT <aleks167943@yandex.ru>
2019-12-12 21:18:56 +0300

    Test 1 (def pop()) # 1

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -24,27 +24,28 @@ def push(elem: Any) -> None:
 
 
 
-def pop() -> Any:
-	"""
-	Pop element from the top of the stack. If not elements - should return None.
+def pop(staсk) -> Any:
+    """
+    Pop element from the top of the stack. If not elements - should return None.
 
-	:return: popped element
-	"""
+    :return: popped element
+    """
+    print('Удалили элемент {} из стека.'.format(elem))
 
-    print('Удалили элемент {} из стека.'.format())
-
-	return None
+    global staсk
+    staсk.pop()
+    return None
 
 
 def peek(ind: int = 0) -> Any:
-	"""
-	Allow you to see at the element in the stack without popping it.
+    """
+    Allow you to see at the element in the stack without popping it.
 
-	:param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
-	:return: peeked element or None if no element in this place
-	"""
-	print(ind)
-	return None
+    :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
+    :return: peeked element or None if no element in this place
+    """
+    print(ind)
+    return None
 
 
 def clear() -> None:
@@ -56,5 +57,5 @@ def clear() -> None:
 	return None
 
 if __name__ == '__main__':
-	push(1)
+	pop(1)
 	print(staсk)
