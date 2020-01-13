41c144b5b238298c9cc60fc9dc89a5e0eed8863d
ALELAT <aleks167943@yandex.ru>
2020-01-13 19:12:26 +0300

    G0. Пузырьковая сортировка (неправильные табы, поправить)

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -12,19 +12,22 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	"""
 	container
 
+	replace = 0
 	for i in range(len(container) - 1):
 		for j in range(len(container) - 1 - i):
 			if container[j] > container[j+1]:
 				container[j], container[j + 1] = container[j + 1], container[j]
-
-
-
-	return container
+                replace += 1
+                print('Count replace', replace)
+                replace = 0
+                return container
 
 if __name__ == '__main__':
-	import random
-	N = 5
-	# shuffle_list = list(range(N))
-	# random.shuffle(shuffle_list)
-	shuffle_list = [1, 0, 4, 2, 3]
-	print(sort(shuffle_list))
+    import random
+    N = 5
+    # shuffle_list = list(range(N))
+    # random.shuffle(shuffle_list)
+    shuffle_list = [4, 3, 2, 1, 0]
+    # shuffle_list = [1, 0, 4, 2, 3]
+    # shuffle_list = [0, 1, 2, 4, 5]
+    print(sort(shuffle_list))
